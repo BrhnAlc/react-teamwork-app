185ac986aa4bc5b99cc465151bb1648a585d6489
burhan <alacaburhan122016@gmail.com>
2023-07-19 17:28:33 +0300

    ONCLİCK

--- src/components/NameGetter.jsx
+++ src/components/NameGetter.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
 
 
 
 
 const NameGetter = () => {
+
+    const [name,setName]=useState(0);
+    const handleChange =(e)=>{
+        setName(e.target.value)
+    }
+    
   return (
     <div className="text-center mt-4 ">
 
 <div>isminizi giriniz: </div>
-    <input type="text"  /> 
-
-    <div>isminiz:</div>   
+    <input type="text" value={name} onChange={handleChange} /> 
+    <button onClick={()=>setName("")}>temizle</button>
+    <div>isminiz:{name}</div>   
     </div>
   )
 }
 
-export default NameGetter
+export default NameGetter;
