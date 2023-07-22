a03f6666c06dfcfb784dce41f82b9eccb2513c33
burhan <alacaburhan122016@gmail.com>
2023-07-22 23:19:07 +0300

    Page Title Change

--- src/components/Counterr.jsx
+++ src/components/Counterr.jsx
@@ -1,26 +1,21 @@
-import { useState } from "react"
-
+import { useEffect ,useState } from "react";
 
 
 
 const Counterr = () => {
 
-    const [ count, setCount] = useState(0)
-    const handleClick=(e)=>{
-        setCount(count + 1)
-    }
+const [ count, setCount] = useState(0);
+
+ useEffect=(()=>{
+    document.title =`click ${count} times`
+ }, [count])
+console.log(count);
   return (
-    <div>
-        
-        
-  <p>buttona tıklandı:{count}</p>  
-
-  <button onClick={handleClick}>artır</button>
-
-  <button onClick={()=>setCount(count - 2)}>azalt</button>
-        
-        
-</div>
+    <div>   
+<h1>Page Title Change</h1>
+<p>You clicked {count} times.</p>
+<button onClick={()=>setCount(count + 1)}>Click me</button>
+    </div>
   )
 }
 
